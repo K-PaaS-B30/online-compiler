7a74e72036cf6fd59f3e54df445c54af479aa9b7
NotableDeveloper <castlereal01@gmail.com>
2024-08-01 00:41:27 +0900

    refactor : CompileResponseDto 경과 시간 수정
    nano 시간에서 소수점 네 자리의 second를 반환하도록 수정

--- src/main/java/b3o/onlinecompiler/dto/response/CompileResponseDto.java
+++ src/main/java/b3o/onlinecompiler/dto/response/CompileResponseDto.java
@@ -4,18 +4,23 @@ import b3o.onlinecompiler.entity.ContainerContext;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.text.DecimalFormat;
+
 @Getter
 @Builder
 public class CompileResponseDto {
     private String status;
-    private long durationTime;
+    private String durationTime;
     private String stdout;
     private String stderr;
 
     public static CompileResponseDto fromContext(ContainerContext context){
+        double second = ((double) context.getStartTime() - (double) context.getEndTime()) / 1_000_000_000.0;
+        DecimalFormat df = new DecimalFormat("#.####");
+
         return CompileResponseDto.builder()
                 .status(context.getStatus().name())
-                .durationTime(context.getEndTime() - context.getStartTime())
+                .durationTime(df.format(Math.abs(second)))
                 .stdout(context.getContainerStdout().toString())
                 .stderr(context.getContainerStderr().toString())
                 .build();
